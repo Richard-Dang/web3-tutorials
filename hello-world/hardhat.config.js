2beb3037d5d679b0b4537a63140db1b7d7afe31b
Richard Dang <richard@alchemy.com>
2023-08-29 16:10:14 -0700

    Complete part 4 tutorial

--- hello-world/hardhat.config.js
+++ hello-world/hardhat.config.js
@@ -4,8 +4,9 @@
 
 require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.7.3",
@@ -17,5 +18,8 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`],
       gasPrice: 35000000000,
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
